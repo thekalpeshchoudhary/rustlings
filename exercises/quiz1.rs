79c3d534b9746f0a9845bd04faa1b269a63d2f8b
Kalpesh Choudhary <53808244+thekalpeshchoudhary@users.noreply.github.com>
2024-01-27 10:54:34 +0530

    exercises done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(price: u32) -> u32 {
+    if price <= 40 {
+        price * 2
+    } else {
+        price
+    }
+}
 
 // Don't modify this function!
 #[test]
